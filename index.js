9009d0177f97f9ef82bce979f813d06710cc3426
MaLiHa1321 <malihasultana2001@gmail.com>
2023-11-04 22:20:53 +0600

    server site intialization and mongo setup are complete

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+
+// middelware
+app.use(cors())
+app.use(express.json())
+
+
+// libraryManage
+// rdRKtcVeejWcSafK
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ntnzcww.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("libraryManage");
+    const brandCollection = database.collection("categories");
+
+
+    // get the categories 
+    app.get('/category', async(req,res) =>{
+const cursor = brandCollection.find()
+  const result = await cursor.toArray()
+  res.send(result)
+    })
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
